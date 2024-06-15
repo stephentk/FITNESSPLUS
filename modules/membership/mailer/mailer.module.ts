e87890c6af1d7b60ba0e0ccc59da319e6ef8b202
stephen kofoworola <84783886+stephentk@users.noreply.github.com>
2024-06-15 08:31:10 +0100

    Initial commit

--- modules/membership/mailer/mailer.module.ts
+++ modules/membership/mailer/mailer.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MailerService } from './mailer.service';
 import { EmailController } from './mailer.controller';
+import { MembershipService } from '../membership.service';
+import { membershipProviders } from '../membership.provider';
 
 
 @Module({
-  providers: [MailerService],
+  providers: [MailerService,MembershipService,...membershipProviders],
   controllers: [EmailController],
   exports: [MailerService],
 })
